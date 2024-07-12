f0f7b5b26f6dd071412f6c167e2db2b91a407ed2
Anderson Freires <55098476+dre36dre@users.noreply.github.com>
2024-07-12 01:29:38 +0000

    Iterando elementos com Iterator

---
+++ Iterator.java
@@ -0,0 +1,12 @@
+import java.util.Map;
+
+public class TesteIterator{
+    public static void main(String[] args){
+        Map<String,Integer> map=new HashMap<>();
+        map.put("Pedro", 4);
+        map.put("Maria", 9);
+        map.put("Hugo", 12);
+
+        System.out.println(map);
+    }
+}
